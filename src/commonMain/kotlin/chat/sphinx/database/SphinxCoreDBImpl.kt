c0402004069b29ef6c72022211ad171cda5b68d4
kngako <kpangako+github@gmail.com>
2022-06-17 00:24:52 +0200

    Fix sqlite db locked bug
    https://github.com/stakwork/sphinx-kotlin-ui/issues/45

--- src/commonMain/kotlin/chat/sphinx/database/SphinxCoreDBImpl.kt
+++ src/commonMain/kotlin/chat/sphinx/database/SphinxCoreDBImpl.kt
@@ -4,18 +4,20 @@ import chat.sphinx.concepts.authentication.encryption_key.EncryptionKey
 import chat.sphinx.features.coredb.CoreDBImpl
 import chat.sphinx.utils.build_config.BuildConfigDebug
 import com.squareup.sqldelight.db.SqlDriver
+import kotlinx.atomicfu.locks.SynchronizedObject
+import kotlinx.atomicfu.locks.synchronized
 
 class SphinxCoreDBImpl(
     private val driverFactory: DriverFactory,
     private val buildConfigDebug: BuildConfigDebug,
 ): CoreDBImpl() {
-
+    private val lock = SynchronizedObject()
 
     @Volatile
     private var driver: SqlDriver? = null
 
     override fun getSqlDriver(encryptionKey: EncryptionKey): SqlDriver {
-        return driver ?: synchronized(this) {
+        return synchronized(lock) {
             driver ?: driverFactory.createDriver(encryptionKey)
                 .also { driver = it }
         }
