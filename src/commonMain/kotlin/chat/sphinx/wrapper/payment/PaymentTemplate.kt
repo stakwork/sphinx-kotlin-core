244af0afd9ac280649b050fc55e70db16b822f5f
Tom <tomasalbertotiminskas@gmail.com>
2022-09-01 11:30:23 -0300

    LocalFile added on PaymentTemplate class

--- src/commonMain/kotlin/chat/sphinx/wrapper/payment/PaymentTemplate.kt
+++ src/commonMain/kotlin/chat/sphinx/wrapper/payment/PaymentTemplate.kt
@@ -1,11 +1,15 @@
 package chat.sphinx.wrapper.payment
 
+import java.io.File
+
 class PaymentTemplate(
     val muid: String,
     val width: Int,
     val height: Int,
     val token: String
 ) {
+
+    var localFile: File? = null
 
     fun getTemplateUrl(mediaHost: String): String {
         return "https://$mediaHost/template/$muid"
